e0469820c5fbdc37425eda812d5bab5ddacc5f64
German Grandson <ganieto1@gmail.com>
2016-05-24 15:42:28 -0400

    create link views and other functions

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     url(r'^$', views.index, name ='index'),
     #127.0.0.1/polls
 
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    #127.0.0.1/polls/1
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name='detail'),
+    #127.0.0.1/polls/1/results
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='detail'),
+    #127.0.0.1/polls/1/vote
+
 ]
